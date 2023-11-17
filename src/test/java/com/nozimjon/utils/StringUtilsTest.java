41234e220fe0de5ca5cda45dce90611a6f55a454
Nozimjon Nabiev <nozimjonnabiev7@gmail.com>
2023-11-17 21:38:39 +0500

    Refactor StringUtils for better readability and maintainability

--- src/test/java/com/nozimjon/utils/StringUtilsTest.java
+++ src/test/java/com/nozimjon/utils/StringUtilsTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class StringUtilsTest {
     @Test
-    public void testIsPositiveNumber() {
+    public void isPositiveNumber() {
         assertTrue(StringUtils.isPositiveNumber("10"));
         assertFalse(StringUtils.isPositiveNumber("-5"));
         assertFalse(StringUtils.isPositiveNumber("Hello World!"));
